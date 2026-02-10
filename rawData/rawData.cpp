777683bdcf6801605e44cd7b79c850e92bed94ff
Sophia Duan <sophiayduan@gmail.com>
2026-02-10 16:03:01 -0500

    add rawData file
    code on esp32

---
+++ rawData/rawData.cpp
@@ -0,0 +1,32 @@
+#include <WiFi.h>
+#include <WebSocketsClient.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "BELL227";
+const char* password = "3DE33F2FE311";
+
+WebSocketsClient webSocket;
+
+#define HALL_PIN 35
+bool sent = false;
+
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) { delay(500); }
+  webSocket.begin("192.168.2.189", 8080, "/");
+}
+
+void loop() {
+  webSocket.loop();
+
+  JsonDocument doc;
+  doc["analog"] = analogRead(0);
+  doc["hall_mT"] = analogRead(HALL_PIN);
+  String jsonString;
+  serializeJson(doc, jsonString);
+  webSocket.sendTXT(jsonString);
+
+  delay(10); // send every 10ms
+}
